51ebd840d5c48ef9d75663685aa0e403dc0f0d1e
halimaxo <halimakshuchi@gmail.com>
2023-06-30 18:54:16 -0500

    post tutoring update

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,7 @@ var mainEl = document.getElementById("main");
 
 var startBtn = document.querySelector(".start-button");
 
-var secondsLeft = 3;
+var secondsLeft = 30;
 
 var pageOne = document.querySelector(".startContainer");
 
@@ -23,56 +23,63 @@ questionBox.style.display = "none";
 finishedQuiz.style.display = "none";
 
 var questions = [
-    {
-        Text: "What is your favorite color?", 
-        Choices: ["red", "blue", "lilac", "pink"], 
-        Correct: "lilac"
-    },
-    {
-        Text: "What is the best state?",
-        Choices: ["Illinois", "Texas", "California", "New York"],
-        Correct: "Illinois"
-    },
-    {
-        Text: "What is the best food?",
-        Choices: ["Korean", "Iraqi", "Bengali", "Yemeni"],
-        Correct: "Bengali"
-    },
-]
+  {
+    Text: "What is your favorite color?",
+    Choices: ["red", "blue", "lilac", "pink"],
+    Correct: "lilac",
+  },
+  {
+    Text: "What is the best state?",
+    Choices: ["Illinois", "Texas", "California", "New York"],
+    Correct: "Illinois",
+  },
+  {
+    Text: "What is the best food?",
+    Choices: ["Korean", "Iraqi", "Bengali", "Yemeni"],
+    Correct: "Bengali",
+  },
+];
 
 function setTime() {
-    var timerInterval = setInterval ( function() {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft + " seconds left till quiz finishes.";
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left till quiz finishes.";
 
-        if(secondsLeft < 0) {
-            clearInterval(timerInterval);
-            sendMessage();
-        }
-    }, 1000);
+    if (secondsLeft < 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+  }, 1000);
 }
 
 function sendMessage() {
-    timeEl.textContent = " ";
-
+  timeEl.textContent = " ";
 }
 
 function displayQuestion(i) {
-    questionText.textContent = questions[i].Text
-    questions[i].Choices.forEach(
-        function(choice) {
-            var btn = document.createElement("button")
-            btn.textContent = choice
-            answerBox.appendChild(btn)
-        }
-    )
+  questionText.textContent = questions[i].Text;
+  questions[i].Choices.forEach(function (choice) {
+    var btn = document.createElement("button");
+    btn.textContent = choice;
+    answerBox.appendChild(btn);
+    btn.addEventListener("click", function () {
+      verifyAnswer(btn.textContent);
+    });
+  });
+}
+function verifyAnswer(answer) {
+  if (answer === questions[globalIndex].Correct) {
+    console.log("correct");
+  }
+  if (globalIndex !== questions.length) {
+    globalIndex++;
+    displayQuestion(globalIndex);
+  }
 }
 
-startBtn.addEventListener("click", function() {
-    setTime()
-    startBtn.style.display = "none";
-    questionBox.style.display = "block";
-    displayQuestion(globalIndex)
-})
-
-
+startBtn.addEventListener("click", function () {
+  setTime();
+  startBtn.style.display = "none";
+  questionBox.style.display = "block";
+  displayQuestion(globalIndex);
+});
